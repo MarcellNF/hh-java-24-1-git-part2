b795264895c9c0bcb293a523454632ed6d68b8a4
Marcell Dechant <marcell.dechant@neuefische.de>
2024-02-08 10:06:08 +0100

    Update sysout to be Hello Java

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -2,6 +2,6 @@ package de.neuefische;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello neuefische!");
+        System.out.println("Hello Java!");
     }
 }
